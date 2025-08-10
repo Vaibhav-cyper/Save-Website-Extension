94e7461e0d55d658c2fa1499cde6a385aab30fe0
Vaibhav Gupta <gvaibhav2435@gmail.com>
2025-08-10 18:45:15 +0530

    create vite app , install other necessary dependancies

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import manifest from "./manifest.json";
+import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
